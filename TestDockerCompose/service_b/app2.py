af56b175f96b8581a09c567bda6e06e97f3b3c2a
root <root@ip-172-31-4-35.ap-south-1.compute.internal>
2025-02-24 09:32:10 +0000

    arg passing

--- TestDockerCompose/service_b/app2.py
+++ TestDockerCompose/service_b/app2.py
@@ -1,13 +1,24 @@
 
 import requests
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def communicate():
+    print("here i am before")
     response = requests.get("http://service_a:5000")  # Using service name
     return f"Service B received: {response.text}"
+
+@app.route('/otherPage')
+def other_page():
+    return "This is the other page"
+
+@app.route('/arg')
+def Arg():
+    var1 = request.args.get('var1', 'default_value_1')
+    var2 = request.args.get('var2', 'default_value_2')
+    return f"var1: {var1}, var2: {var2}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
